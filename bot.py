2c58876d8650b6f59d15f93e7f55ace235475d81
Oscar Caballero <ocaballeror@gmail.com>
2017-10-21 16:03:19 +0000

    Check for telegram exceptions

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-intro="""Hi there!
+intro = """Hi there!
 
 I'm the lyrics bot, I can search the web and find lyrics for you.
 
@@ -31,17 +31,23 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=intro)
 
 def find(bot, update):
-    res = lyrics.find_lyrics(update.message.text)
-    if res is None:
-        msg = 'Wrong format!'
-    elif res.source is None or res.lyrics=='':
+    res = None
+    try:
+        res = lyrics.find_lyrics(update.message.text)
+
+        if res is None:
+            msg = 'Wrong format!'
+        elif res.source is None or res.lyrics == '':
+            msg = f'Lyrics for {res.artist.title()} - {res.title.title()} could not be found'
+        else:
+            msg = f'''FROM: {lyrics.id_source(res.source, True)}
+
+    {res.lyrics}'''
+
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
+    except Exception:
         msg = f'Lyrics for {res.artist.title()} - {res.title.title()} could not be found'
-    else:
-        msg = f'''FROM: {lyrics.id_source(res.source, True)}
-
-{res.lyrics}'''
-
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+        bot.send_message(chat_id=update.message.chat_id, text=msg)
 
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't"
