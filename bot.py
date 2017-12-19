c07a296c18998c2520153ab2535be0d8a23c0497
Oscar Caballero <ocaballeror@gmail.com>
2017-12-19 20:42:00 +0000

    Some stylistic changes thanks to pylint

--- bot.py
+++ bot.py
@@ -1,36 +1,39 @@
 import logging
+import json
 import lyricfetch.lyrics as lyrics
 import telegram
-import json
 
-from db import DB
+from db import DB as Database
 from lyricfetch.lyrics import Result, Song
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
 
 HELPFILE = './help.txt'
 CONFFILE = './config.json'
 
-intro = ""
-try:
-    helpfile = open(HELPFILE, 'r')
-    intro = helpfile.read()
-    helpfile.close()
-except Exception as e:
-    logging.exception(e)
+DB = None
 
 def start(bot, update):
     '''Function to be called on /start commmand'''
+    intro = ""
+    try:
+        helpfile = open(HELPFILE, 'r')
+        intro = helpfile.read()
+        helpfile.close()
+    except Exception as e:
+        logging.exception(e)
+
     send_message(intro, bot, update.message.chat_id)
 
 def other(bot, update):
-    last_res = db.get_last_res(update.message.chat_id)
+    """Use a different source to find lyrics for the last searched song"""
+    last_res = DB.get_last_res(update.message.chat_id)
     if last_res:
         song = Song.from_info(artist=last_res[0], title=last_res[1])
-        result = Result(song, last_res[2])
         sources = lyrics.exclude_sources(last_res[2], True)
-        msg, valid = get_lyrics(song, update.message.chat_id, sources)
+        msg, _ = get_lyrics(song, update.message.chat_id, sources)
     else:
         msg = "You haven't searched for anything yet"
 
@@ -43,7 +46,7 @@ def get_lyrics(song, chat_id, sources=None):
     valid = False
     try:
         res = None
-        if type(song) is str:
+        if isinstance(song, str):
             song = Song.from_string(song)
 
         if song:
@@ -59,21 +62,23 @@ def get_lyrics(song, chat_id, sources=None):
         else:
             msg = 'FROM: {}\n\n{}'.format(lyrics.id_source(res.source, True).lower(), song.lyrics)
             valid = True
-            db.log_result(chat_id, res)
+            DB.log_result(chat_id, res)
     except Exception as e:
-        print(e)
+        logging.exception(e)
         msg = 'Unknown error'
 
     return msg, valid
 
 def find(bot, update):
-    lyrics, _ = get_lyrics(update.message.text, update.message.chat_id)
-    send_message(lyrics, bot, update.message.chat_id)
+    """Find lyrics for a song"""
+    lyrics_str, _ = get_lyrics(update.message.text, update.message.chat_id)
+    send_message(lyrics_str, bot, update.message.chat_id)
 
 def send_message(msg, bot, chat_id):
+    """Splits a string into MAX_LENGTH chunks and sends them as messages"""
     try:
         last_section = 0
-        chunksize=telegram.constants.MAX_MESSAGE_LENGTH
+        chunksize = telegram.constants.MAX_MESSAGE_LENGTH
         for section in range(chunksize, len(msg), chunksize):
             section = msg.rfind('\n\n', last_section, section)
 
@@ -98,6 +103,7 @@ def send_message(msg, bot, chat_id):
         bot.send_message(chat_id=chat_id, text=msg)
 
 def unknown(bot, update):
+    """Fallback function for commands that don't match any of the known ones"""
     send_message("Sorry, I didn't understand that command", bot,
             update.message.chat_id)
 
@@ -105,8 +111,8 @@ def parse_config():
     """Returns a dictionary with all the necessary data from the configuration
     file"""
     try:
-        with open(CONFFILE, 'r') as config:
-            data = json.load(config)
+        with open(CONFFILE, 'r') as conffile:
+            data = json.load(conffile)
             required_keys = ['token', 'dbuser', 'dbname', 'dbpassword']
             for key in required_keys:
                 if key not in data:
@@ -120,13 +126,15 @@ def parse_config():
 
             return data
     except IOError:
-        logging.critical('Could not read the configuration file '+CONFFILE)
+        logging.critical('Could not read the configuration file %s', CONFFILE)
         return None
 
-if __name__ == '__main__':
+def main():
+    global DB
+
     config = parse_config()
     if not config:
-        exit(1)
+        return 1
 
     updater = Updater(config['token'])
     updater.dispatcher.add_handler(CommandHandler('start', start))
@@ -134,11 +142,16 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(MessageHandler(Filters.text, find))
     updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
-    db = DB(config['dbname'], config['dbuser'], config['dbpassword'],
+    DB = Database(config['dbname'], config['dbuser'], config['dbpassword'],
             config['dbhost'])
     updater.start_polling()
 
     print('Started')
     updater.idle()
     print('Closing')
-    db.close()
+    DB.close()
+
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
