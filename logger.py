8bd233a1e92f3b7ebd73df4743ecf3baa6ae2fc1
Oscar Caballero <ocaballeror@tutanota.com>
2019-05-12 19:09:35 +0000

    Log to console and file

--- logger.py
+++ logger.py
@@ -1,7 +1,13 @@
 import logging
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - ' '%(levelname)s - %(message)s'
-)
+fmt = '%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s'
+formatter = logging.Formatter(fmt)
 logger = logging.getLogger('bot')
 logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('lyrics-bot.log', mode='a')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
