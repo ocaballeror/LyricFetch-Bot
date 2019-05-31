9662d5f9bda0d952c8efb8150f52cd6111201efc
Oscar Caballero <ocaballeror@tutanota.com>
2019-05-31 13:19:22 +0000

    Set default debug level to INFO

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging
 fmt = '%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s'
 formatter = logging.Formatter(fmt)
 logger = logging.getLogger('bot')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler('lyrics-bot.log', mode='a')
 file_handler.setFormatter(formatter)
